6bd434f008096a0f62de2653a62ce1c9e2cc1b5c
francescoP02 <file.partipilo@gmail.com>
2022-06-07 15:33:12 +0200

    Added paragraph with and without censured word with paragraph lenght

--- index.php
+++ index.php
@@ -7,6 +7,49 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+
+
+
+<?php
+    //dichiariamo le variabili
+
+    $text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus rerum pariatur corrupti nostrum dignissimos, laudantium, officia facilis et exercitationem obcaecati quam similique sit cupiditate nobis minima labore ipsum eveniet ut?";
+
+?>
+
+<h1>Il testo è:</h1>
+<p>
+    <?php 
+    //testo non modificato
+    echo $text 
+    ?>
+</p>
+
+<h4>La lunghezza del testo è di:</h4>
+<span>
+    <?php
+    //lunghezza del testo
+    echo strlen($text)
+    ?>
+</span>
+
+<?php $badword = $_GET["badword"]; ?>
+
+<h4>La parola da censurare è:</h4>
+<span> 
+    <?php
+    //parola da rimuovere nel testo
+    echo $badword 
+    ?> 
+</span>
+
+<?php
+    $censured = str_replace($badword, "***", $text); 
+    // un esempio è http://localhost/php-badwords/?badword=sit
+?>
+
+<h2>Il paragrafo censurato è:</h2>
+<p><?php echo $censured ?></p>
+
 </body>
 </html>
